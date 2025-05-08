f85e4a94c38b980403bcbb8c1840547a8e965370
DaddyGao <hezy906@gmail.com>
2025-05-07 21:23:40 -0700

    add module5_oop.py

---
+++ module5_oop.py
@@ -0,0 +1,48 @@
+class NumberList:
+	def __init__(self):
+		self.numbers = []
+
+	def insert(self, number):
+		self.numbers.append(number)
+
+	def search(self, target):
+		try:
+			return self.numbers.index(target) + 1
+		except ValueError:
+			return -1
+
+
+def main():
+	while True:
+		try:
+			N = int(input("Enter a positive integer N: "))
+			if (N <= 0):
+				raise ValueError
+			break
+		except ValueError:
+			print("Please enter a valid positive integer.")
+
+	number_list = NumberList()
+
+	for i in range(N):
+		while True:
+			try:
+				num = int(input(f"Enter number {i + 1}: "))
+				number_list.insert(num)
+				break
+			except ValueError:
+				print("Please enter a valid integer.")
+
+	while True:
+		try:
+			X = int(input("Enter the number X to search for: "))
+			break
+		except ValueError:
+			print("Please enter a valid integer.")
+
+	position = number_list.search(X)
+	print(position)
+
+
+if __name__ == "__main__":
+	main()
